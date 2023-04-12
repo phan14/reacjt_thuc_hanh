400f467b931afa026a50189627c2f87c38a5f98f
đình trần <trandangdinh@dtu.edu.vn>
2023-04-12 09:02:27 +0700

    ss1

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./components/pages/login";
+
+const Layout = () => {
+  return <>main page</>;
+};
+
+export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <div>404 not found</div>,
+    },
+
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router} />;
+    </>
+  );
+}
